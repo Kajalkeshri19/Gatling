468d0a2702fb17f0a9850dbffc05efd6766b7d37
Kajal <kajal.kumari@knoldus.com>
2023-05-12 22:39:02 +0530

    Updated

--- README.md
+++ README.md
@@ -1,14 +1,31 @@
-Project Name:
-Gatling Assignment
+Project Name: Gatling Assignment
 
-Project Description:
-Creating a gatlin project using maven using all the http method i.e get, post and put. All the data inputs are fetching from external csv file. Assertions have been used.
+Project Description: Creating a gatling project using maven to create virtual users and making request using all the http methods i.e get, post and put. All the data inputs are fetching from external csv file. Assertions have been used.
 
-Installation:
-installing gatling and then created a maven project with gatling highchart maven artifact and then scala sdk is downloaded.
+Steps to set up Gatling project using maven:
 
-Usage:
-You can run the load tests by Gatling engine or by terminal by command “mvn gatling:test”.
+Create a new Project in intellij or any preferred IDE
+Choose MavenArchetype.
+Go to maven repository and search for Gatling high-chart maven Archetype.
+Now copy the group id and Artifact id and paste it in the maven archetype group id and archetype ID and Create it.
+Now You will get the template. Now create a scala file and add scala sdk plugin.
+Now Create a file by .scala and write your code.
+At last add the scala sdk plugin.
 
-Codes:
-For codes you can see the scala file.
+Plugin Used:
+Scala SDK.
+
+Technologies used: 
+Programming language – Scala 
+Build tool – Maven 
+Load Testing Tool – Gatling 
+IDE – Intellij
+
+Steps for execution:
+
+Gatling Engine: You can directly run the code by Gatling Engine.
+Terminal: Run the code by terminal by using the command “mvn gatling: test -Dsec=5 -DusersAtOnce=50 -DconstantUsers=20 -DrampUsers=100
+
+Codes: For codes you can see the .scala file.
+
+Output Report: For detailed understanding of output refer the OutputReport.doc in the repository.
--- src/test/scala/simulations/Gatling.scala
+++ src/test/scala/simulations/Gatling.scala
@@ -1,22 +1,26 @@
 package simulations
 
-
 import io.gatling.core.scenario.Simulation
 import io.gatling.core.Predef._
+import io.gatling.core.feeder.BatchableFeederBuilder
+import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
-
+import io.gatling.http.protocol.HttpProtocolBuilder
 
 class Gatling extends Simulation {
+  val sec: Int = Integer.getInteger("sec", 5)
+  val userAtOnce: Int = Integer.getInteger("usersAtOnce", 50)
+  val constantUser: Int = Integer.getInteger("constantUsers", 20)
+  val rampUser: Int = Integer.getInteger("rampUsers", 100)
+  val user: BatchableFeederBuilder[String]#F = csv("src/test/resources/body/AddUser.csv").random
+  val updateUser: BatchableFeederBuilder[String]#F = csv("src/test/resources/body/UpdateUser.csv").circular
 
-  val user = csv("src/test/resources/body/AddUser.csv").random
-  val updateUser=csv("src/test/resources/body/UpdateUser.csv").circular
   //http conf
-  val httpConf = http.baseUrl("https://reqres.in")
+  val httpConf: HttpProtocolBuilder = http.baseUrl("https://reqres.in")
     .header("content-type", "application/json")
 
-
   //Scenario
-  val scn = scenario("Scenario 1")
+  val scn: ScenarioBuilder = scenario("Scenario 1")
 
     //Get Request
     .exec(http("Get user request").get("/api/users?page=2").check(status is 200))
@@ -29,8 +33,8 @@ class Gatling extends Simulation {
     .feed(updateUser)
     .exec(http("Put Request").put("/api/users/2").body(StringBody("""{"name":"${name}", "job":"${job}"}""")).asJson.check(status is 200))
 
-    //Setup
-    setUp(scn.inject(nothingFor(5),atOnceUsers(50),constantUsersPerSec(20).during(15),rampUsers(100).during(30))).maxDuration(60).protocols(httpConf).assertions(forAll.responseTime.max.lt(2200), forAll.successfulRequests.count.lte(450),global.failedRequests.count.is(0L))
+  //Setup
+  setUp(scn.inject(nothingFor(sec), atOnceUsers(userAtOnce), constantUsersPerSec(constantUser).during(15), rampUsers(rampUser).during(30))).maxDuration(60).protocols(httpConf).assertions(forAll.responseTime.max.lt(2500), forAll.successfulRequests.count.gte(0), global.failedRequests.count.is(0L))
 
 
 }
